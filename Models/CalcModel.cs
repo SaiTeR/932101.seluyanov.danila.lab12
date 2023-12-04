1b612d19d5b22ec80dba4571ca8160a2e22a24c8
Danila Seluyanov <dan.seluyanov@gmail.com>
2023-12-04 23:04:44 +0700

    Initial commit

---
+++ Models/CalcModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab12.Models
+{
+    public class CalcModel
+    {
+        public string calcName { get; set; }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public string operation { get; set; }
+        public string controllerMethod { get; set; }
+
+    }
+}
